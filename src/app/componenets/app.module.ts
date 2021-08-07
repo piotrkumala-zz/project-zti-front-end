b0810738de57bba221449ae664716d012a187044
piotrkumala <piotrek.kumala@gmail.com>
2021-08-07 11:58:36 +0200

    move header to separate module

--- src/app/componenets/app.module.ts
+++ src/app/componenets/app.module.ts
@@ -1,41 +1,26 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { HeaderComponent } from './header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { StoreModule } from '@ngrx/store';
 import { surveyReducer } from '../store/survey/survey.reducer';
 import { idReducer } from '../store/id/id.reducer';
 import { RouterModule, Routes } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { SurveySearchDialogComponent } from './header/survey-search-dialog/survey-search-dialog.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
+import { HeaderModule } from './header/header.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const routes: Routes = [];
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SurveySearchDialogComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
     RouterModule.forRoot(routes),
     StoreModule.forRoot({ survey: surveyReducer, id: idReducer }),
-    MatButtonModule,
-    MatIconModule,
-    MatDialogModule,
-    MatFormFieldModule,
-    FormsModule,
-    MatInputModule,
-    ReactiveFormsModule,
+    HeaderModule,
   ],
   providers: [{ provide: LOCALE_ID, useValue: 'pl' }],
   bootstrap: [AppComponent],
