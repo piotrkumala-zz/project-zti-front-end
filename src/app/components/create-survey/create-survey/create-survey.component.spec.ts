602ab9a6be4ae5f0112019b42cdc40e6a83a4b72
piotrkumala <piotrek.kumala@gmail.com>
2021-08-25 20:13:27 +0200

    fix tests

--- src/app/components/create-survey/create-survey/create-survey.component.spec.ts
+++ src/app/components/create-survey/create-survey/create-survey.component.spec.ts
@@ -1,17 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateSurveyComponent } from './create-survey.component';
-import { provideMockStore } from '@ngrx/store/testing';
+import { MatTreeModule } from '@angular/material/tree';
+import { CreateSurveyLeafComponent } from './create-survey-leaf/create-survey-leaf.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 describe('CreateSurveyComponent', () => {
   let component: CreateSurveyComponent;
   let fixture: ComponentFixture<CreateSurveyComponent>;
 
   beforeEach(async () => {
-    const initialState = { survey: {}, id: '', newSurvey: {} };
     await TestBed.configureTestingModule({
-      declarations: [CreateSurveyComponent],
-      providers: [provideMockStore({ initialState })],
+      declarations: [CreateSurveyComponent, CreateSurveyLeafComponent],
+      imports: [
+        MatTreeModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+        MatIconModule,
+      ],
     }).compileComponents();
   });
 
