5898bbeb52de5b331c762e25eeec1516ebcd70d4
piotrkumala <piotrek.kumala@gmail.com>
2021-08-29 23:15:22 +0200

    add tests of header and app components

--- src/app/components/app.component.spec.ts
+++ src/app/components/app.component.spec.ts
@@ -1,17 +1,32 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HeaderComponent } from './header/header.component';
-import { provideMockStore } from '@ngrx/store/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MatDialog } from '@angular/material/dialog';
 import { AnswerSurveyComponent } from './answer-survey/answer-survey/answer-survey.component';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatIcon } from '@angular/material/icon';
 import { MatStepper } from '@angular/material/stepper';
+import { SurveyControllerService } from '../api/services';
+import createSpyObj = jasmine.createSpyObj;
+import { Store, StoreModule } from '@ngrx/store';
+import { setId } from '../store/id/id.actions';
+import { reducers } from '../store/state';
+import { of } from 'rxjs';
+import { ClientSurvey } from '../api/models';
 
 describe('AppComponent', () => {
-  const initialState = { survey: {}, id: '', newSurvey: {} };
+  const mockSurveyService = createSpyObj('SurveyControllerService', {
+    getSurvey: of<ClientSurvey>({
+      description: 'test',
+      title: 'test',
+      question: [],
+      rootQuestion: 'sadfasdf',
+    }),
+  });
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
@@ -23,16 +38,44 @@ describe('AppComponent', () => {
         MatStepper,
       ],
       providers: [
-        provideMockStore({ initialState }),
         { provide: MatDialog, useValue: {} },
+        { provide: SurveyControllerService, useValue: mockSurveyService },
       ],
-      imports: [HttpClientTestingModule, RouterTestingModule],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        StoreModule.forRoot(reducers),
+      ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should get survey info when id is set in store', () => {
+    const mockStore = TestBed.inject(Store);
+    mockStore.dispatch(setId({ id: 'test' }));
+
+    expect(mockSurveyService.getSurvey).toHaveBeenCalledWith({
+      id: 'test',
+    });
+  });
+
+  it('should set survey after info is obtained from api', async () => {
+    const mockStore = TestBed.inject(Store);
+    mockStore.dispatch(setId({ id: 'test' }));
+
+    mockStore.select('survey').subscribe((x) => {
+      expect(x).toEqual({
+        description: 'test',
+        title: 'test',
+        question: [],
+        rootQuestion: 'sadfasdf',
+      });
+    });
   });
 });
--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -2,24 +2,39 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
 import { MatDialog } from '@angular/material/dialog';
-import { provideMockStore } from '@ngrx/store/testing';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatIcon } from '@angular/material/icon';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Store, StoreModule } from '@ngrx/store';
+import { reducers } from '../../store/state';
+import createSpyObj = jasmine.createSpyObj;
+import { of } from 'rxjs';
+import { Routes } from '@angular/router';
+import { AnswerSurveyComponent } from '../answer-survey/answer-survey/answer-survey.component';
+import { CreateSurveyComponent } from '../create-survey/create-survey/create-survey.component';
+import { ThankYouForAnswerComponent } from '../answer-survey/thank-you-for-answer/thank-you-for-answer.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
-  const initialState = { survey: {}, id: '', newSurvey: {} };
+  const mockDialog = createSpyObj('MatDialog', {
+    open: createSpyObj('MatDialog', { afterClosed: of('test') }),
+  });
+  const routes: Routes = [
+    { path: 'answer', component: AnswerSurveyComponent },
+    { path: 'create', component: CreateSurveyComponent },
+    { path: '', redirectTo: '/answer', pathMatch: 'full' },
+    { path: 'thanks', component: ThankYouForAnswerComponent },
+  ];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HeaderComponent, MatToolbar, MatIcon],
-      providers: [
-        { provide: MatDialog, useValue: {} },
-        provideMockStore({ initialState }),
+      providers: [{ provide: MatDialog, useValue: mockDialog }],
+      imports: [
+        RouterTestingModule.withRoutes(routes),
+        StoreModule.forRoot(reducers),
       ],
-      imports: [RouterTestingModule],
     }).compileComponents();
   });
 
@@ -31,5 +46,14 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set survey id after exiting dialog', () => {
+    component.searchSurvey();
+
+    const store = TestBed.inject(Store);
+    store.select('id').subscribe((x) => {
+      expect(x).not.toBeNull();
+    });
   });
 });
